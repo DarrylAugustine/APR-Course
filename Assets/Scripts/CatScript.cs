1d69a01bf0f3b9f6cdbff77ba5945e85c72a368c
DarrylAugustine <78181668+DarrylAugustine@users.noreply.github.com>
2021-05-18 20:15:36 -0700

    Initial commit

---
+++ Assets/Scripts/CatScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Animator catAnimationController;
+    public AudioClip meowSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Fish"))
+            catAnimationController.SetBool("FishTrigger", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Fish"))
+
+            catAnimationController.SetBool("FishTrigger", false);
+
+    }
+}
---
+++ Assets/Scripts/FishScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishScript : MonoBehaviour
+{
+
+
+    [SerializeField] private Animator fishAnimationController;
+    public AudioClip meowSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Cat"))
+        fishAnimationController.SetBool("CatTrigger", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Cat"))
+           
+        fishAnimationController.SetBool("CatTrigger", false);
+        
+    }
+   
+
+}
+
+
