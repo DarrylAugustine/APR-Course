5be8a3d44c420dac7baa546e19f3c14d68f732eb
DarrylAugustine <78181668+DarrylAugustine@users.noreply.github.com>
2021-05-20 10:02:43 -0700

    End of Lesson 7

---
+++ Assets/Scripts/OrganList.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class OrganList : MonoBehaviour
+{
+
+    public Image brainIcon;
+    public Image heartIcon;
+    public Image bladderIcon;
+    public Image lungIcon;
+    public Image stomachIcon;
+
+    /*public UnityEvent findOrgan;
+
+    public void OnOrganFound()
+    {
+        findOrgan.Invoke();
+    }
+    */
+
+    public void ChangeButtonColor()
+    {
+
+        //GetComponent<Image>().color = Color.white;
+
+
+
+
+    }
+    public void OnBrainFound()
+    {
+        brainIcon.color = Color.white;
+    }
+
+    public void OnLungFound()
+    {
+        lungIcon.color = Color.white;
+    }
+
+    public void OnBladderFound()
+    {
+        bladderIcon.color = Color.white;
+    }
+
+    public void OnHeartFound()
+    {
+        heartIcon.color = Color.white;
+    }
+
+    public void OnStomachFound()
+    {
+        stomachIcon.color = Color.white;
+    }
+}
